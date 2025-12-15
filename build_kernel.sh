8115c35b4fc3d6272ec63366723ed04e7bec144b
/ˈmuːn.laɪt/ <anntuongg@gmail.com>
2025-12-15 22:17:51 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export CROSS_COMPILE=/home/luminous418/zyc-clang/bin/aarch64-linux-gnu-
-export LD=/home/luminous418/zyc-clang/bin/ld.lld
-export OBJCOPY=/home/luminous418/zyc-clang/bin/llvm-objcopy
-export AS=/home/luminous418/zyc-clang/bin/llvm-as
-export NM=/home/luminous418/zyc-clang/bin/llvm-nm
-export STRIP=/home/luminous418/zyc-clang/bin/llvm-strip
-export OBJDUMP=/home/luminous418/zyc-clang/bin/llvm-objdump
-export READELF=/home/luminous418/zyc-clang/bin/llvm-readelf
-export CC=/home/luminous418/zyc-clang/bin/clang
-export CROSS_COMPILE_ARM32=/home/luminous418/zyc-clang/bin/arm-linux-gnueabi-
+export CROSS_COMPILE=$HOME/zyc-clang/bin/aarch64-linux-gnu-
+export LD=$HOME/zyc-clang/bin/ld.lld
+export OBJCOPY=$HOME/zyc-clang/bin/llvm-objcopy
+export AS=$HOME/zyc-clang/bin/llvm-as
+export NM=$HOME/zyc-clang/bin/llvm-nm
+export STRIP=$HOME/zyc-clang/bin/llvm-strip
+export OBJDUMP=$HOME/zyc-clang/bin/llvm-objdump
+export READELF=$HOME/zyc-clang/bin/llvm-readelf
+export CC=$HOME/zyc-clang/bin/clang
+export CROSS_COMPILE_ARM32=$HOME/zyc-clang/bin/arm-linux-gnueabi-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
@@ -19,7 +19,7 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j$(nproc) && make -C $(pwd) O=$(pwd)/out KCFLAGS='-w -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j$(nproc)
 clear
-make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc) a32_noksu_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc) a32_k+e_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
 
 #to copy all the kernel modules (.ko) to "modules" folder.
