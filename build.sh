6af0ebcde56e3eec864f87e25f205572dfa7d167
asuka ;) <anntuongg@gmail.com>
2025-12-26 20:20:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+info() {
 # ==========================================================
 #  Clang & defconfig info
 # ==========================================================
@@ -21,7 +22,9 @@ export ANDROID_MAJOR_VERSION=r
 export KCFLAGS=' -w -pipe -O3'
 export KCPPFLAGS=' -O3'
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+}
 
+start_build() {
 echo "                              _         "
 echo " _   ______ _____ ___  ____  (_)_______ "
 echo "| | / / __ `/ __ `__ \/ __ \/ / ___/ _ \ "
@@ -33,21 +36,30 @@ echo "Have a nice day, $USER!"
 
 echo "Infomation:"
 echo "kernel: vampire | version: 4.14.356 | config: $defconfig"
+}
 
+clang() {
 echo "Cloning zyc clang version 14"
+    [ -d ./clang ] || {
+        echo "Clang not found!"
+    }
 git clone https://github.com/EmanuelCN/zyc_clang-14 clang
+}
 
+build() {
 echo "Building, please wait."
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j$(nproc) && make -C $(pwd) O=$(pwd)/out KCFLAGS='-w -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j$(nproc)
 clear
 make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc) $defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=' -w -pipe -O3' CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
+}
 
 #to copy all the kernel modules (.ko) to "modules" folder.
 #mkdir -p modules
 #find . -type f -name "*.ko" -exec cp -n {} modules \;
 #echo "Module files copied to the 'modules' folder."
+copy() {
 echo "Creating out folder in your home."
 mkdir $HOME/out
 
@@ -56,3 +68,4 @@ cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
 cp out/arch/arm64/boot/Image $HOME/out
 
 echo "Build done! Your out image file ís on $HOME/out"
+}
